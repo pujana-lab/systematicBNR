2b1b9f30ad887bcfc45139eeb243a3c0998712f9
luis palomero <luis@declarando.es>
2020-06-03 18:23:12 +0200

    Build base code for running a clean check

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(inmuneAssociation)
+
+test_check("inmuneAssociation")
---
+++ tests/testthat/test-hello.R
@@ -0,0 +1,4 @@
+library(testthat)
+test_that("multiplication works", {
+  expect_equal(hello(), 'Hello')
+})
